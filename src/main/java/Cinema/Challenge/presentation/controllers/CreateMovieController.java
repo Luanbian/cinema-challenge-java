82088d09f947cc5ad1428a6d356d3dfcc57c9abc
luan <luanbianchine1@gmail.com>
2024-01-12 13:41:03 -0300

    feat: add controller to create movie

---
+++ src/main/java/Cinema/Challenge/presentation/controllers/CreateMovieController.java
@@ -0,0 +1,28 @@
+package Cinema.Challenge.presentation.controllers;
+
+import Cinema.Challenge.core.DTOs.MovieDto;
+import Cinema.Challenge.data.interfaces.ICreate;
+import Cinema.Challenge.domain.entities.Movie;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class CreateMovieController {
+    private final ICreate<Movie, MovieDto> create;
+
+    @Autowired
+    public CreateMovieController(ICreate<Movie, MovieDto> create) {
+        this.create = create;
+    }
+
+    @PostMapping(value = "/movies")
+    public ResponseEntity handle(@RequestBody MovieDto movieDto) {
+        Movie movie = this.create.perform(movieDto);
+        return ResponseEntity.ok(movie);
+    }
+}
