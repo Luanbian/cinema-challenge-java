a150fadd5d9cef76238dbd6f400054847c36aaae
luan <luanbianchine1@gmail.com>
2024-01-12 13:58:27 -0300

    test: ensure the movie are created with correct values

--- src/test/java/Cinema/Challenge/data/usecases/CreateMovieTest.java
+++ src/test/java/Cinema/Challenge/data/usecases/CreateMovieTest.java
@@ -4,20 +4,27 @@ import Cinema.Challenge.core.DTOs.MovieDto;
 import static org.junit.jupiter.api.Assertions.*;
 
 import Cinema.Challenge.domain.entities.Movie;
+import InMemory.Movie.CreateInMemory;
 import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
 
 public class CreateMovieTest {
+    private final CreateMovie sut;
+
+    public CreateMovieTest () {
+        CreateInMemory repository = new CreateInMemory();
+        this.sut = new CreateMovie(repository);
+    }
+
     @Test
     public void should_be_able_to_create_movie_with_correct_params() {
-        CreateMovie sut = new CreateMovie();
         MovieDto data = new MovieDto(
                 "Title_test",
                 "synopsis_test",
                 Optional.of("release_test")
         );
-        Movie result = sut.perform(data);
+        Movie result = this.sut.perform(data);
         assertNotNull(result);
         assertNotNull(result.getId());
         assertNotNull(result.getCreatedAt());
