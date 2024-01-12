9259b1061b7e69d317983c9eda95c3dbc4a009db
luan <luanbianchine1@gmail.com>
2024-01-12 12:06:48 -0300

    feat: inject adapter in usecase

--- src/main/java/Cinema/Challenge/data/usecases/CreateMovie.java
+++ src/main/java/Cinema/Challenge/data/usecases/CreateMovie.java
@@ -3,16 +3,22 @@ package Cinema.Challenge.data.usecases;
 import Cinema.Challenge.core.DTOs.MovieDto;
 import Cinema.Challenge.data.interfaces.ICreate;
 import Cinema.Challenge.domain.entities.Movie;
+import Cinema.Challenge.infra.interfaces.ICreateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Optional;
 
 public class CreateMovie implements ICreate<Movie, MovieDto> {
+    @Autowired
+    ICreateRepository<Movie> repository;
+
     @Override
     public Movie perform(MovieDto data) {
         String _title = data.title();
         String _synopsis = data.synopsis();
         Optional<String> _releaseDate = data.releaseDate();
         Movie movie = Movie.create(_title, _synopsis, _releaseDate);
+        this.repository.create(movie);
         return movie;
     }
 }
